ea40d7c59c0311c77a7d889460697343c89ccf5c
Arthur <arthurdlima99@gmail.com>
2022-04-17 22:38:57 -0300

    global index/css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TrabalhosComponent } from './views/trabalhos/trabalhos.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:"trabalhos", component: TrabalhosComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TrabalhosComponent } from './views/trabalhos/trabalhos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TrabalhosComponent
   ],
   imports: [
     BrowserModule,
